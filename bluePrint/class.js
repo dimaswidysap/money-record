0812fd41ff027181ea899c6ee1611b62a7020fba
DimasWds <dimaswidysaputra41@gmail.com>
2026-03-25 18:00:10 +0700

    d

--- bluePrint/class.js
+++ bluePrint/class.js
@@ -67,16 +67,14 @@ class Wallet {
       "list-desc-money-expenditure",
     );
 
-    const allTransactionsIncome = this.filterIncome(this.transactions);
-    const allTransactionsExpenditure = this.filterExpenditure(
+    const allTransactionsIncome = this.filterType(this.transactions, "income");
+    const allTransactionsExpenditure = this.filterType(
       this.transactions,
+      "expenditure",
     );
 
-    this.displayListIncome(containerListIncome, allTransactionsIncome);
-    this.displayListExpenditure(
-      containerListExpenditure,
-      allTransactionsExpenditure,
-    );
+    this.displayList(containerListIncome, allTransactionsIncome);
+    this.displayList(containerListExpenditure, allTransactionsExpenditure);
   }
 
   get totalBalance() {
@@ -99,11 +97,8 @@ class Wallet {
     return income.reduce((curr, acc) => acc.amount + curr, 0);
   }
 
-  filterIncome(list) {
-    return list.filter((income) => income.type === "income");
-  }
-  filterExpenditure(list) {
-    return list.filter((income) => income.type !== "income");
+  filterType(list, type) {
+    return list.filter((income) => income.type === type);
   }
 
   saveToLocalStorageCurrenUser() {
@@ -127,60 +122,84 @@ class Wallet {
     localStorage.setItem("wallets", JSON.stringify(wallets));
   }
 
-  displayListIncome(containerList, lists) {
+  displayList(containerList, lists) {
     containerList.innerHTML = "";
 
     lists.forEach((items) => {
       let idList = items.id;
 
-      const list = document.createElement("li");
-      const span1 = document.createElement("span");
-      const span2 = document.createElement("span");
-      const div = document.createElement("div");
-      const buttonDelete = document.createElement("button");
-
-      list.classList.add("containerLists");
-
-      buttonDelete.textContent = "Delete";
-
-      containerList.appendChild(list);
-      list.append(span1, span2, div);
-      div.append(buttonDelete);
-
-      span1.textContent = items.description;
-      span2.textContent = `Rp ${items.amount.toLocaleString()}`;
-
-      buttonDelete.addEventListener("click", () => {
-        this.deleteTransaction(idList);
-      });
-    });
-  }
-  displayListExpenditure(containerList, lists) {
-    containerList.innerHTML = "";
-
-    lists.forEach((items) => {
-      let idList = items.id;
+      // console.log(lists);
 
       const list = document.createElement("li");
       const span1 = document.createElement("span");
       const span2 = document.createElement("span");
       const div = document.createElement("div");
-      const buttonDelete = document.createElement("button");
 
-      list.classList.add("containerLists");
+      Object.assign(div.style, {
+        margin: "1.5rem 0 0 0",
+        display: "flex",
+        gap: "0.5rem",
+      });
 
-      buttonDelete.textContent = "Delete";
+      const button = ["Delete", "Edit"];
 
-      containerList.appendChild(list);
-      list.append(span1, span2, div);
-      div.append(buttonDelete);
+      button.forEach((items, index) => {
+        const buttonDelete = document.createElement("button");
+        const conIconDelet = document.createElement("div");
+        const iconDelete = document.createElement("img");
 
-      span1.textContent = items.description;
+        list.classList.add("containerLists");
+
+        buttonDelete.textContent = items;
+        iconDelete.setAttribute(
+          "src",
+          index === 0 ? "asset/icon/delete.svg" : "asset/icon/edit.svg",
+        );
+
+        containerList.appendChild(list);
+        list.append(span1, span2, div);
+        div.append(buttonDelete);
+        buttonDelete.appendChild(conIconDelet);
+        conIconDelet.appendChild(iconDelete);
+
+        Object.assign(iconDelete.style, {
+          width: "100%",
+          objectFit: "cover",
+        });
+
+        Object.assign(buttonDelete.style, {
+          display: "flex",
+          alignItems: "center",
+          gap: "0.5rem",
+          flexDirection: "row-reverse",
+          cursor: "pointer",
+          backgroundColor: "var(--secondary)",
+          padding: "0.5rem",
+          borderRadius: "10px",
+          fontWeight: "800",
+          color: "var(--white)",
+          boxShadow: " rgba(0, 0, 0, 0.24) 0px 3px 8px",
+        });
+
+        Object.assign(conIconDelet.style, {
+          height: "1.3rem",
+          padding: "0.2rem",
+          aspectRatio: "1/1",
+          backgroundColor: index === 0 ? "orange" : "rgb(0, 193, 0)",
+          borderRadius: "50%",
+        });
+
+        if (index === 0) {
+          buttonDelete.addEventListener("click", () => {
+            this.deleteTransaction(idList);
+          });
+        }
+      });
+
+      span1.textContent = items.description + " ";
       span2.textContent = `Rp ${items.amount.toLocaleString()}`;
 
-      buttonDelete.addEventListener("click", () => {
-        this.deleteTransaction(idList);
-      });
+      // console.log(list);
     });
   }
 }
